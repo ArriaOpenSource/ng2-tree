6e8cd3b48d26816514ac7798e168c13612b38fdb
Jonathan Henderson <jonathan.henderson@arria.com>
2022-03-28 14:15:49 +0100

    Upgrade to angular 13
    - Remove "conventional" dependencies

--- src/demo/app/app.component.ts
+++ src/demo/app/app.component.ts
@@ -712,7 +712,7 @@ export class AppComponent implements OnInit {
     AppComponent.logEvent(e, 'Created');
   }
 
-  public onNodeFFSCreated(e: NodeEvent, controller): void {
+  public onNodeFFSCreated(e: NodeEvent, controller?): void {
     AppComponent.logEvent(e, 'Created');
     if (controller) {
       controller.changeNodeId(++this.lastFFSNodeId);
